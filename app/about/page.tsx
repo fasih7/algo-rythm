2a366412eab9c12ff48ff4121ba92440155174d6
fasih7 <kahloon17@gmail.com>
2025-04-20 17:12:31 +0500

    initial commit with project ready

---
+++ app/about/page.tsx
@@ -0,0 +1,28 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About PageS",
+  description: "This is About Page for Startup Nextjs Template",
+  // other metadata
+};
+
+const AboutPage = () => {
+  return (
+    <>
+      <Breadcrumb
+        pageName="About Page"
+        description="We help businesses unlock their full digital potential by
+                  delivering reliable, high-performance software solutions
+                  tailored to your needs."
+      />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+    </>
+  );
+};
+
+export default AboutPage;
---
+++ components/Brands/brandsData.tsx
@@ -0,0 +1,41 @@
+import { Brand } from "@/types/brand";
+
+const brandsData: Brand[] = [
+  {
+    id: 1,
+    name: "UIdeck",
+    href: "",
+    image: "/images/brands/next-long.png",
+    imageLight: "/images/brands/uideck-light.svg",
+  },
+  {
+    id: 2,
+    name: "Tailgrids",
+    href: "",
+    image: "/images/brands/react-long.png",
+    imageLight: "/images/brands/react.png",
+  },
+  {
+    id: 3,
+    name: "Lineicons",
+    href: "",
+    image: "/images/brands/node.png",
+    imageLight: "/images/brands/lineicons-light.svg",
+  },
+  {
+    id: 4,
+    name: "Ayro UI",
+    href: "",
+    image: "/images/brands/aws.png",
+    imageLight: "/images/brands/ayroui-light.svg",
+  },
+  {
+    id: 5,
+    name: "PlainAdmin",
+    href: "",
+    image: "/images/brands/google-cloud.png",
+    imageLight: "/images/brands/plainadmin-light.svg",
+  },
+];
+
+export default brandsData;
